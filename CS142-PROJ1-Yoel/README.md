c1be76e0f7440122f13f2fafb997d2057b772432
yoelba <benavry@clarkson.edu>
2016-03-07 16:08:15 -0500

    Added files via upload

---
+++ CS142-PROJ1-Yoel/README.md
@@ -0,0 +1,2 @@
+# CS142-PROJ1
+The bleeding edge of technological innovation
---
+++ CS142-PROJ1-Yoel/test.cpp
@@ -0,0 +1,13 @@
+// test.cpp
+//
+// Launches the text editor
+
+#include "FileViewer.h"
+
+int main()
+{
+    FileViewer viewer;
+    viewer.run();
+
+    return 0;
+}
